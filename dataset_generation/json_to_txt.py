2932d2820592178694a8d330952e75789c063a5d
Antton Lamarka <Tony@MacBook-Pro-de-Antton.local>
2020-04-20 11:16:18 +0200

    Correct missalignment issue

--- dataset_generation/json_to_txt.py
+++ dataset_generation/json_to_txt.py
@@ -1,5 +1,7 @@
 ##This script takes a JSON file of a paper (wish I knew the name of the format)
 #and makes it into a txt file, puting a newline each paragraph.
+#It also creates a 'rebuild_reference.txt' file, necessary at the end of the BioBERT pipeline I created.
+#This file contains information not given by the BioBERT output but necessary in the PubAnnotation JSON files.
 
 import sys
 import json
@@ -22,20 +24,24 @@ with open('generated_text.txt', 'w') as out_file:
                 try:  # This avoids the first line and names that I don't have files for
                     with open(mypath + paper + '.json', 'r') as json_file:
                         data = json.load(json_file) #Dictionary containing the json file
-                        if data["metadata"]:  # If there is a title
+                        if data["metadata"]['title']:  # If there is a title. Might be better to extract title and abstract from metafile. Some of the JSONs seem incomplete
                             title = data['metadata']['title']
                             out_file.write(title + '\n\n')
                             divid += 1
                             out_reference.write(line[0] + '-' + str(divid) + '-title '\
                             + line[0] + ' ' + line[2] + ' ' + line[5] + ' ' + str(divid) + '\n')
                         if data['abstract']:  # "If there is an abstract"
-                            if isinstance(data['abstract'],list):
-                                data['abstract'] = data['abstract'][0]
-                            abstract = data['abstract']['text']
-                            out_file.write(abstract + '\n\n')
-                            divid += 1
-                            out_reference.write(line[0] + '-' + str(divid) + '-abstract '\
-                            + line[0] + ' ' + line[2] + ' ' + line[5] + ' ' + str(divid) + '\n')
+                            for paragraph in data['abstract']:
+                                '''
+                                if isinstance(data['abstract'],list): # If data[abstract] is a list
+                                    data['abstract'] = data['abstract'][0]
+                                abstract = data['abstract']['text']
+                                out_file.write(abstract + '\n\n')
+                                '''
+                                out_file.write(paragraph['text'] + '\n\n')
+                                divid += 1
+                                out_reference.write(line[0] + '-' + str(divid) + '-abstract '\
+                                + line[0] + ' ' + line[2] + ' ' + line[5] + ' ' + str(divid) + '\n')
                         for sentence in data['body_text']:
                             out_file.write(sentence['text'] + '\n\n')
                             divid += 1
